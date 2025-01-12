dd9a151387670acbda8642e07d48527055205871
weeger <romain.weeger@gmail.com>
2025-01-12 15:08:49 +0100

    Create mixin

---
+++ wexample_filestate_dev/workdir/mixins/with_readme_workdir_mixin.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from wexample_config.const.types import DictConfig
+from wexample_filestate.config_value.readme_content_option_value import ReadmeContentConfigValue
+from wexample_filestate.const.disk import DiskItemType
+
+
+class WithReadmeWorkdirMixin:
+    def append_readme(self, config: Optional[DictConfig] = None) -> DictConfig:
+        children = config["children"]
+
+        children.append({
+            "name": 'README.md',
+            "type": DiskItemType.FILE,
+            "should_exist": True,
+            "default_content": ReadmeContentConfigValue(
+                templates=[],
+                parameters={}
+            ),
+        })
+
+        return config
