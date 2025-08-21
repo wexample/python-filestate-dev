4e3d43705788d94beb227ca6c049df2664080236
weeger <romain.weeger@gmail.com>
2025-08-21 18:29:14 +0200

    Rectify

--- wexample_filestate_dev/workdir/mixins/with_readme_workdir_mixin.py
+++ wexample_filestate_dev/workdir/mixins/with_readme_workdir_mixin.py
@@ -1,24 +1,27 @@
 from typing import Optional
 
 from wexample_config.const.types import DictConfig
-from wexample_filestate.config_option.text_filter_config_option import TextFilterConfigOption
-from wexample_filestate.config_value.readme_content_config_value import ReadmeContentConfigValue
+from wexample_filestate.config_option.text_filter_config_option import (
+    TextFilterConfigOption,
+)
+from wexample_filestate.config_value.readme_content_config_value import (
+    ReadmeContentConfigValue,
+)
 from wexample_filestate.const.disk import DiskItemType
 
 
 class WithReadmeWorkdirMixin:
     def append_readme(self, config: Optional[DictConfig] = None) -> DictConfig:
-        config.get("children").append({
-            "name": 'README.md',
-            "type": DiskItemType.FILE,
-            "should_exist": True,
-            "default_content": ReadmeContentConfigValue(
-                templates=[],
-                parameters={}
-            ),
-            "text_filter": [
-                TextFilterConfigOption.OPTION_NAME_ENSURE_NEWLINE
-            ],
-        })
+        config.get("children").append(
+            {
+                "name": "README.md",
+                "type": DiskItemType.FILE,
+                "should_exist": True,
+                "default_content": ReadmeContentConfigValue(
+                    templates=[], parameters={}
+                ),
+                "text_filter": [TextFilterConfigOption.OPTION_NAME_ENSURE_NEWLINE],
+            }
+        )
 
         return config
--- wexample_filestate_dev/workdir/mixins/with_version_workdir_mixin.py
+++ wexample_filestate_dev/workdir/mixins/with_version_workdir_mixin.py
@@ -1,22 +1,24 @@
 from typing import Optional, Any
 
 from wexample_config.const.types import DictConfig
-from wexample_filestate.config_option.text_filter_config_option import TextFilterConfigOption
+from wexample_filestate.config_option.text_filter_config_option import (
+    TextFilterConfigOption,
+)
 from wexample_filestate.const.disk import DiskItemType
 from wexample_helpers.helpers.string import string_ensure_end_with_new_line
 
 
 class WithVersionWorkdirMixin:
     def append_version(self, config: Optional[DictConfig] = None) -> DictConfig:
-        config.get("children").append({
-            "name": 'version.txt',
-            "type": DiskItemType.FILE,
-            "should_exist": True,
-            "default_content": self._get_version_default_content(),
-            "text_filter": [
-                TextFilterConfigOption.OPTION_NAME_ENSURE_NEWLINE
-            ],
-        })
+        config.get("children").append(
+            {
+                "name": "version.txt",
+                "type": DiskItemType.FILE,
+                "should_exist": True,
+                "default_content": self._get_version_default_content(),
+                "text_filter": [TextFilterConfigOption.OPTION_NAME_ENSURE_NEWLINE],
+            }
+        )
 
         return config
 
