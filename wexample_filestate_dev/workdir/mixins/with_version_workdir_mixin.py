82b37cd9d3f9ed89a609617f149e3d3a39d36697
weeger <romain.weeger@gmail.com>
2025-08-21 12:39:21 +0200

    Build default version

--- wexample_filestate_dev/workdir/mixins/with_version_workdir_mixin.py
+++ wexample_filestate_dev/workdir/mixins/with_version_workdir_mixin.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Any
 
 from wexample_config.const.types import DictConfig
 from wexample_filestate.config_option.text_filter_config_option import TextFilterConfigOption
@@ -11,10 +11,13 @@ class WithVersionWorkdirMixin:
             "name": 'version.txt',
             "type": DiskItemType.FILE,
             "should_exist": True,
-            "default_content": f"0.0.1",
+            "default_content": self._get_version_default_content(),
             "text_filter": [
                 TextFilterConfigOption.OPTION_NAME_ENSURE_NEWLINE
             ],
         })
 
         return config
+
+    def _get_version_default_content(self) -> Any:
+        return "0.0.1"
